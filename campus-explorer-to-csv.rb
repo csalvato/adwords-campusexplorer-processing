4c980ccdfa90eb5a449705eda2aeb76943e4f806
Chris Salvato <csalvato@gmail.com>
2014-12-07 22:59:03 +0000

    added function for process_source codes

--- campus-explorer-to-csv.rb
+++ campus-explorer-to-csv.rb
@@ -13,25 +13,7 @@ def process_ce_csv (input_filename, output_filename)
 		# Process the utm_campaign string as passed through from Source Code into separate values in their own cells
 		# Is there data?
 			if has_campusexplorer_data? row
-				puts row["Source Code"]
-					# utm_campaign = _src*adwords_x*205882121_d*mb_d2*{ifmobile:mb}{ifnotmobile:dt}_k*{keyword}_m*{matchtype}_c*{creative}_p*{adposition}_n*{network}&utm_source=Google&utm_medium=cpc
-						# => Area of Study
-						# => concentration
-						# => seed
-						# => sublocation
-						# => Location Code
-						# => headline
-						# => Broken data? (Does it have {} in the URL?)
-						# => LP Parameter
-						# => Source Parameter
-						# => Campaign ID (and decode back to campaign name)
-						# => Desktop/Mobile values
-						# => Second Desktop/Mobile Value
-						# => Keyword
-						# => Match Type?
-						# => creative
-						# => Ad position
-						# => Network
+				process_source_code row["Source Code"]
 				# Write ALL values out to processed CSV file
 			end
 		end
@@ -61,6 +43,28 @@ def get_output_filename
 	end
 	return filename
 end	
+
+def process_source_code (sourcecode)
+	
+	# utm_campaign = _src*adwords_x*205882121_d*mb_d2*{ifmobile:mb}{ifnotmobile:dt}_k*{keyword}_m*{matchtype}_c*{creative}_p*{adposition}_n*{network}&utm_source=Google&utm_medium=cpc
+		# => Area of Study
+		# => concentration
+		# => seed
+		# => sublocation
+		# => Location Code
+		# => headline
+		# => Broken data? (Does it have {} in the URL?)
+		# => LP Parameter
+		# => Source Parameter
+		# => Campaign ID (and decode back to campaign name)
+		# => Desktop/Mobile values
+		# => Second Desktop/Mobile Value
+		# => Keyword
+		# => Match Type?
+		# => creative
+		# => Ad position
+		# => Network
+end
 
 input_filename = get_input_filename
 output_filename = get_output_filename
