295065ae4b768c8b5173597821069d48286ead64
Chris Salvato <csalvato@gmail.com>
2014-12-07 22:57:21 +0000

    Added gitignore

--- campus-explorer-to-csv.rb
+++ campus-explorer-to-csv.rb
@@ -5,36 +5,43 @@ puts "Starting Script..."
 
 require 'csv'
 
+# Processes a CE data **CSV** File into output
 def process_ce_csv (input_filename, output_filename)
 	CSV.open(output_filename, "wb") do |csv|
 	# For each row from Campus Explorer CSV File
 		CSV.foreach(input_filename, :headers => true, :return_headers => false, :encoding => 'windows-1251:utf-8') do |row|
 		# Process the utm_campaign string as passed through from Source Code into separate values in their own cells
 		# Is there data?
-			# => YES -> If there's revenue (maybe it's if there is any utm_campaign string appended at all?)
-				# utm_campaign = _src*adwords_x*205882121_d*mb_d2*{ifmobile:mb}{ifnotmobile:dt}_k*{keyword}_m*{matchtype}_c*{creative}_p*{adposition}_n*{network}&utm_source=Google&utm_medium=cpc
-					# => Area of Study
-					# => concentration
-					# => seed
-					# => sublocation
-					# => Location Code
-					# => headline
-					# => Broken data? (Does it have {} in the URL?)
-					# => LP Parameter
-					# => Source Parameter
-					# => Campaign ID (and decode back to campaign name)
-					# => Desktop/Mobile values
-					# => Second Desktop/Mobile Value
-					# => Keyword
-					# => Match Type?
-					# => creative
-					# => Ad position
-					# => Network
-			# Write ALL values out to processed CSV file
+			if has_campusexplorer_data? row
+				puts row["Source Code"]
+					# utm_campaign = _src*adwords_x*205882121_d*mb_d2*{ifmobile:mb}{ifnotmobile:dt}_k*{keyword}_m*{matchtype}_c*{creative}_p*{adposition}_n*{network}&utm_source=Google&utm_medium=cpc
+						# => Area of Study
+						# => concentration
+						# => seed
+						# => sublocation
+						# => Location Code
+						# => headline
+						# => Broken data? (Does it have {} in the URL?)
+						# => LP Parameter
+						# => Source Parameter
+						# => Campaign ID (and decode back to campaign name)
+						# => Desktop/Mobile values
+						# => Second Desktop/Mobile Value
+						# => Keyword
+						# => Match Type?
+						# => creative
+						# => Ad position
+						# => Network
+				# Write ALL values out to processed CSV file
+			end
 		end
-	puts "#{input_filename}"
-	puts "#{output_filename}"
 	end
+end
+
+def has_campusexplorer_data? (row)
+	# => YES -> If there's revenue and a source code (maybe it's if there is any utm_campaign string appended at all?)	
+	row["Unreconciled Publisher Total Revenue"].to_f > 0 &&
+	!row["Source Code"].empty?
 end
 
 def get_input_filename
@@ -53,7 +60,7 @@ def get_output_filename
 		filename = gets.chomp
 	end
 	return filename
-end
+end	
 
 input_filename = get_input_filename
 output_filename = get_output_filename
